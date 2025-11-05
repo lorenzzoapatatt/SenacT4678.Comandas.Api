cb6a2de681597ed193bc4cc4071a0b6b481bc3c0
LORENZZOANTONIOPATAT <LORENZZOANTONIOPATAT@SCS217758>
2025-11-04 21:47:30 -0300

    botando update-database

--- Comandas Api/Comandas.Api/ComandasDbContext.cs
+++ Comandas Api/Comandas.Api/ComandasDbContext.cs
@@ -7,6 +7,45 @@ namespace Comandas.Api
         public ComandasDbContext(DbContextOptions<ComandasDbContext> options) : base(options)
         {
         }
+        // Definir algumas configurações adicionais no banco
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Models.Usuario>()
+                .HasData(
+                    new Models.Usuario
+                    {
+                        Id = 1,
+                        Nome = "Admin",
+                        Email = "admin@admin.com",
+                        Senha = "admin123"
+                    }
+                );
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Models.Mesa>()
+                .HasData(
+                    new Models.Mesa
+                    {
+                        Id = 2,
+                        NumeroMesa = 1,
+                        SituacaoMesa = 1
+                    }
+                );
+            modelBuilder.Entity<Models.Mesa>();
+
+            modelBuilder.Entity<Models.CardapioItem>()
+                .HasData(
+                    new Models.CardapioItem
+                    {
+                        Id = 3,
+                        Titulo = "",
+                        Descricao = "",
+                        Preco = 10,
+                        PossuiPreparo = true,
+                    };
+            modelBuilder.Entity<Models.CardapioItem>();
+        }
+
         public DbSet<Models.Usuario> Usuarios { get; set; } = default!;
         public DbSet<Models.Mesa> Mesas { get; set; } = default!;
         public DbSet<Models.Reserva> Reservas { get; set; } = default!;
